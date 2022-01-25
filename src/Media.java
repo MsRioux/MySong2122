d564151a040074a43cdf0968ef09391670f00de6
MsRioux <jrioux@mvusd.net>
2022-01-25 08:16:08 -0800

    Added Media Interface

---
+++ src/Media.java
@@ -0,0 +1,11 @@
+public interface Media
+{
+    public String name = "";
+    public double time = 0; //duration in minutes
+
+    public String getName();
+    public void setName(String name);
+
+    public double getTime();
+    public void setTime(double time);
+}
